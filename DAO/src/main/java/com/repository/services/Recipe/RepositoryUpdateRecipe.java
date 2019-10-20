17f0637ffc5fba6c526b331d442c4896c49478d9
Karol Kuchnio <kuchniokarol@gmail.com>
2019-10-20 12:00:48 +0200

    Update RepositoryUpdateRecipe.java
    update

--- DAO/src/main/java/com/repository/services/Recipe/RepositoryUpdateRecipe.java
+++ DAO/src/main/java/com/repository/services/Recipe/RepositoryUpdateRecipe.java
@@ -3,5 +3,5 @@ package com.repository.services.Recipe;
 import bslogic.model.Recipe;
 
 public interface RepositoryUpdateRecipe {
-    Recipe updateFullRecipe(Recipe recipe,Integer id);
+    Recipe updateFullRecipe(Recipe recipe);
 }
